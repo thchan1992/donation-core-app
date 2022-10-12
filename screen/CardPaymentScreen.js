380e8f5fa6e5f86b696b912163db033d19f72eaa
Han Chan <chantszhang@gmail.com>
2022-10-12 19:23:33 +0100

    created the screen that takes donation payment

---
+++ screen/CardPaymentScreen.js
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import NumberInput from "../components/NumberInput";
+import PrimaryButton from "../components/PrimaryButton";
+
+const CardPaymentScreen = ({ navigation, route }) => {
+  const { charity, donateAmount } = route.params;
+  const [cardNum, setCardNum] = useState(null);
+  const [cvv, setCvv] = useState(null);
+  const [expDate, setExpDate] = useState(null);
+  const handlePayment = () => {
+    console.log(charity);
+    console.log(cardNum, "\n", cvv, "\n", expDate, "\n");
+  };
+  return (
+    <View style={styles.container}>
+      <Text>
+        Your donation amount to {charity.name} is: {donateAmount}
+      </Text>
+      <NumberInput
+        value={cardNum}
+        setter={setCardNum}
+        placeholder={"Card Number"}
+      />
+      <NumberInput value={cvv} setter={setCvv} placeholder={"CVV"} />
+      <NumberInput
+        value={expDate}
+        setter={setExpDate}
+        placeholder={"EXP DATE"}
+      />
+      <PrimaryButton text="Confirm" onPress={handlePayment} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
+
+export default CardPaymentScreen;
