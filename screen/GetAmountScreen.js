347e4e8f70197bdf6e6317e4e655e7b3ba22e950
Han Chan <chantszhang@gmail.com>
2022-10-12 19:04:20 +0100

    create a screen that takes user's donation amount

---
+++ screen/GetAmountScreen.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const GetAmountScreen = ({ navigation, route }) => {
+  const [donateAmount, setDonateAmount] = useState(null);
+  const { charity } = route.params;
+
+  const handleSubmit = () => {
+    console.log(
+      "go to the card payment screen with the amount: ",
+      donateAmount
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>How much would you like to donate to {charity.name}?</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={setDonateAmount}
+          style={styles.input}
+          value={donateAmount}
+          placeholder="Donation Amount"
+          keyboardType="numeric"
+        />
+        <PrimaryButton text={"Donate"} onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default GetAmountScreen;
