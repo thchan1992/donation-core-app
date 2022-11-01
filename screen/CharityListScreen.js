cdbc528e07725eaa65ac9a5fa1f5aaa1623e4b21
Han Chan <chantszhang@gmail.com>
2022-11-01 10:15:14 +0000

    added scrollview

--- screen/CharityListScreen.js
+++ screen/CharityListScreen.js
@@ -1,20 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useEffect } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 const CharityListScreen = ({ navigation, route }) => {
   return (
-    <View>
-      {route.params.charityList.map((obj) => {
-        return (
-          <PrimaryButton
-            text={obj.name}
-            onPress={() => {
-              navigation.navigate("CharityScreen", { charity: obj });
-            }}
-          />
-        );
-      })}
+    <View style={styles.container}>
+      <ScrollView>
+        {route.params.charityList.map((obj) => {
+          return (
+            <PrimaryButton
+              text={obj.name}
+              onPress={() => {
+                navigation.navigate("CharityScreen", { charity: obj });
+              }}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
 export default CharityListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
