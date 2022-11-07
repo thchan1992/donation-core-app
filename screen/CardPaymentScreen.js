c70c476be72cd5a2afef7a3f6ab45469f0162d61
Han Chan <chantszhang@gmail.com>
2022-11-07 22:29:37 +0000

    updated card detail

--- screen/CardPaymentScreen.js
+++ screen/CardPaymentScreen.js
@@ -10,9 +10,9 @@ const CardPaymentScreen = ({ navigation, route }) => {
   const [cardNum, setCardNum] = useState(null);
   const [cvv, setCvv] = useState(null);
   const [expDate, setExpDate] = useState("");
-  const fixCardNum = "1";
-  const fixCvv = "1";
-  const fixExpDate = "1";
+  const fixCardNum = "1234567899990000";
+  const fixCvv = "567";
+  const fixExpDate = "01/02";
 
   const showAlert = () => {
     Alert.alert(
